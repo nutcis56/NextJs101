3f0879ec14707b41c3df155acb749980d2feccca
nutcis56 <nutnon009@hotmail.com>
2026-07-03 09:56:47 +0700

    Add Next.js project

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function DashboardCustomerPage() {
+  return <p>Customer Page</p>;
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function DashboardInvoicePage() {
+  return <p>Invoice Page</p>;
+}
